424bc4ac35177270dcf7960fb759fbe43ee62c64
tb212304_2013 <848967151@qq.com>
2018-08-09 19:49:39 +0800

    first

---
+++ Magic/TabController/MagicTabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MagicTabBarViewController.swift
+//  Magic
+//
+//  Created by Zhang song on 2018/8/9.
+//  Copyright © 2018年 Zhang song. All rights reserved.
+//
+
+import UIKit
+
+class MagicTabBarViewController: UITabBarController {
+
+    
+    //MARK: - life cycle -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+
+}
---
+++ Podfile
@@ -0,0 +1,12 @@
+
+
+platform:ios,'9.0'
+
+target 'Magic' do 
+
+pod 'RNCryptor-objc', '~> 3.0.6'
+pod 'Masonry', '~> 1.1.0'
+
+end
+
+
